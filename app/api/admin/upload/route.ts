360073a39fd6a616a35b8b2d639252935160ecc1
mrkarimoff <mrkarimoff99@gmail.com>
2023-08-09 10:36:33 +0500

    reading json file as utf-8 in frontend sending to backend, checking if the api works in production

--- app/api/admin/upload/route.ts
+++ app/api/admin/upload/route.ts
@@ -1,38 +1,25 @@
 import prisma from "@/lib/prisma";
-import fs, { writeFile } from "fs/promises";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { join } from "path";
 import { options } from "../../auth/[...nextauth]/options";
 
+type JsonFile = {
+  data: string;
+  name: string;
+};
+
 export async function POST(req: Request) {
-  //   const session = await getServerSession(options);
-  //   if (session?.user.role !== "admin")
-  //     return NextResponse.json({ message: "Access Denied" }, { status: 403 });
-
-  const body: FormData = await req.formData();
-  const files: FormDataEntryValue[] = body.getAll("jsonFile");
-
-  const jsonFiles = files.map((item) => item as File);
+  const jsonFiles: JsonFile[] = await req.json();
 
   try {
+    // const session = await getServerSession(options);
+    // if (session?.user.role !== "admin")
+    //   return NextResponse.json({ message: "Access Denied" }, { status: 403 });
+
     if (!jsonFiles.length) return NextResponse.json({ message: "No file here" }, { status: 400 });
 
-    jsonFiles.forEach(async (jsonFile) => {
-      const bytes = await jsonFile.arrayBuffer();
-      const buffer = Buffer.from(bytes);
-
-      const path = join("./public", "uploads", jsonFile.name);
-      await writeFile(path, buffer);
-
-      const data: string = await fs.readFile(path, "utf8");
-      await prisma.jsonData.create({
-        data: {
-          data: data,
-          name: jsonFile.name,
-        },
-      });
-      await fs.unlink(path);
+    await prisma.jsonData.createMany({
+      data: jsonFiles,
     });
 
     return NextResponse.json({ message: "File uploaded successfully!" });
