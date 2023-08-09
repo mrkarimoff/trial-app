cbeebe25a673d951d4c5fb83fd7582c3c504f348
mrkarimoff <mrkarimoff99@gmail.com>
2023-08-09 11:26:04 +0500

    added a loading state

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,18 @@
+const Loading = () => {
+  return (
+    <div className="center">
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
+
+export default Loading;
