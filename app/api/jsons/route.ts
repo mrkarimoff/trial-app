c99e8db4c86433c7a7b3cc5affd926472d8fd564
mrkarimoff <mrkarimoff99@gmail.com>
2023-08-09 14:51:11 +0500

    still fixing deployment error

--- app/api/jsons/route.ts
+++ app/api/jsons/route.ts
@@ -5,8 +5,8 @@ import { options } from "../auth/[...nextauth]/options";
 
 export async function GET(req: Request) {
   try {
-    const session = await getServerSession(options);
-    if (!session) return NextResponse.json({ message: "Access Denied" }, { status: 403 });
+    // const session = await getServerSession(options);
+    // if (!session) return NextResponse.json({ message: "Access Denied" }, { status: 403 });
 
     const jsonData = await prisma.jsonData.findMany();
     return NextResponse.json(jsonData);
