cfc05cbdf5ba768c2cfe61e19c22ffceb550d183
mrkarimoff <mrkarimoff99@gmail.com>
2023-08-09 19:38:52 +0500

    fixed a minor deployment error

--- app/[locale]/jsons/page.tsx
+++ app/[locale]/jsons/page.tsx
@@ -1,10 +1,10 @@
 import JsonsPageContainer from "@/components/main/jsonsPage/jsonsPageContainer";
 import { useTranslations } from "next-intl";
 
-const jsonsPage = () => {
+const JsonsPage = () => {
   const t = useTranslations("JsonsPage");
 
   return <JsonsPageContainer title={t("title")} noFilesMsg={t("noFilesMsg")} />;
 };
 
-export default jsonsPage;
+export default JsonsPage;
