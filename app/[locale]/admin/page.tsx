123732275ed7b2b50505ce40b6da7b9f35f81def
mrkarimoff <mrkarimoff99@gmail.com>
2023-08-09 09:04:16 +0500

    created an api route to handle json uploads

--- app/[locale]/admin/page.tsx
+++ app/[locale]/admin/page.tsx
@@ -1,10 +1,41 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import { FormEvent, useState } from "react";
+
+// import { useTranslations } from "next-intl";
 
 const AdminPage = () => {
-  const t = useTranslations("Index");
+  const [file, setFile] = useState<File>();
+  // const t = useTranslations("Index");
+
+  const sumbitForm = async (e: FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("jsonFile", file as File);
+
+    const data = await fetch(process.env.NEXT_PUBLIC_DOMAIN + "/api/admin/upload", {
+      method: "POST",
+      body: formData,
+    }).then(async (res) => await res.json());
+
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1 className="text-center text-xl font-semibold">{t("admin")}</h1>
+      <h1>Upload JSON</h1>
+      <form onSubmit={sumbitForm}>
+        <input
+          required
+          type="file"
+          onChange={(e) => {
+            if (e.target.files) {
+              setFile(e.target.files[0]);
+            }
+          }}
+        />
+        <button className="p-2 border">save</button>
+      </form>
     </div>
   );
 };
